798a35041777b3e0417291d6f950f75b308d9f91
Wassim Bolles <106280014+WASSIM-BOLLES@users.noreply.github.com>
2023-05-31 12:48:40 +0100

    fix headers mistake

--- M_T_P assets/Wblib.h
+++ M_T_P assets/Wblib.h
@@ -3,5 +3,5 @@ int		wB_is64BitOS();
 char	*wB_strstr(char *str, char *to_find);
 int		wB_strlen(char *str);
 char    *wB_clean_strstr(char *string);
-char    *wB_strcpy(char *dest, char *src)
+char    *wB_strcpy(char *dest, char *src);
 int     wB_sqrt(int nb);
